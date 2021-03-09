cd3669412cb5fd638cfa1db0ebbb99bb3f49e25a
Arne Ludwig <arne.ludwig@posteo.de>
2021-03-09 14:36:06 +0100

    Added `dazzlib.basictype` with semantic basic data type definitions

---
+++ source/dazzlib/basictypes.d
@@ -0,0 +1,22 @@
+/**
+    Aliases for basic data types used in `dazzlib` for better readability.
+
+    Copyright: © 2021 Arne Ludwig <arne.ludwig@posteo.de>
+    License: Subject to the terms of the MIT license, as written in the
+             included LICENSE file.
+    Authors: Arne Ludwig <arne.ludwig@posteo.de>
+*/
+module dazzlib.basictypes;
+
+///
+alias arithmetic_t = int;
+///
+alias coord_t = uint;
+///
+alias diff_t = uint;
+///
+alias id_t = uint;
+///
+alias small_trace_point_t = ubyte;
+///
+alias trace_point_t = ushort;
