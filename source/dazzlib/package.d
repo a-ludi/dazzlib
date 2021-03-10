ed29bdb7fecb29cf07700217a7cd898806df2052
Arne Ludwig <arne.ludwig@posteo.de>
2021-03-10 13:08:34 +0100

    Added root package module

---
+++ source/dazzlib/package.d
@@ -0,0 +1,15 @@
+/**
+    This module publicly imports all high-level interface definitions
+    contained in this library and the `DazzlibException`.
+
+    Copyright: © 2021 Arne Ludwig <arne.ludwig@posteo.de>
+    License: Subject to the terms of the MIT license, as written in the
+             included LICENSE file.
+    Authors: Arne Ludwig <arne.ludwig@posteo.de>
+*/
+module dazzlib;
+
+public import dazzlib.alignments;
+public import dazzlib.basictypes;
+public import dazzlib.db;
+public import dazzlib.util.exception : DazzlibException;
